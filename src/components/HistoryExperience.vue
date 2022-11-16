7671961f43b647e7d9bf1a09ac32200c6cfa1963
nutfyb <56430739+nutfyb@users.noreply.github.com>
2022-11-17 01:32:45 +0700

    edit word time work

--- src/components/HistoryExperience.vue
+++ src/components/HistoryExperience.vue
@@ -215,7 +215,7 @@
                   <div class="ag-timeline-card_inner">
                     <div class="ag-timeline-card_info">
                       <div class="ag-timeline-card_title">
-                        Synergy Corporation  | Jan - Apr 2022
+                        Synergy Corporation  | May - Aug 2022
                       </div>
                       <div class="ag-timeline-card_subtitle">
                         WEB DEVELOPER
